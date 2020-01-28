5ab34907a37f1e87680af59df249d97e78d4afaa
samuel.benning <samuel.benning@zento.fr>
2020-01-28 23:51:17 +0100

    use async / await

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import { apolloServer } from "./apollo";
  * Use an `async` function to get access to `await` keyword
  */
 async function main() {
-
     // The `listen` method launches a web server.
-    apolloServer.listen().then(({ url }) => {
-        L.debug(`Server ready at ${url}`);
-    });
+    const { url } = await apolloServer.listen()
+    L.debug(`Server ready at ${url}`);
 }
 
 /**
